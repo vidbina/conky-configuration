2b1680677208f2a4cd615074d04a7ddc042bfce7
David Asabina <vid@bina.me>
2017-05-14 01:17:06 +0200

    basic bar

--- scripts/helloworld.lua
+++ scripts/helloworld.lua
@@ -14,35 +14,39 @@ function conky_main()
     cr = cairo_create(cs)
     local updates=tonumber(conky_parse('${updates}'))
 
---    -- hello world
---    font="Mono"
---    font_size=12
---    text="helloii world"
---    xpos,ypos=100,100
---    red,green,blue,alpha=1,1,1,1
---    font_slant=CAIRO_FONT_SLANT_NORMAL
---    font_face=CAIRO_FONT_WEIGHT_NORMAL
---
---    cairo_select_font_face (cr, font, font_slant, font_face)
---    cairo_set_font_size (cr, font_size)
---    cairo_set_source_rgba (cr,red,green,blue,alpha)
---
---    cairo_move_to (cr,xpos,ypos);
---    cairo_show_text (cr,text)
---    cairo_stroke (cr)
+    cpu_perc=5*tonumber(conky_parse("${cpu cpu0}"))
+
+    -- hello world
+    font="Mono"
+    font_size=12
+    text="cpu: "..cpu_perc
+    xpos,ypos=100,100
+    red,green,blue,alpha=1,1,1,1
+    font_slant=CAIRO_FONT_SLANT_NORMAL
+    font_face=CAIRO_FONT_WEIGHT_NORMAL
+
+    cairo_select_font_face (cr, font, font_slant, font_face)
+    cairo_set_font_size (cr, font_size)
+    cairo_set_source_rgba (cr,red,green,blue,alpha)
+
+    cairo_move_to (cr,xpos,ypos);
+    cairo_show_text (cr,text)
 
     -- print line
-    line_width=10
+    line_width=2
     line_cap=CAIRO_LINE_CAP_BUTT
     red,green,blue,alpha=1,1,1,1
     startx=100
     starty=100
-    ----------------------------
+    endx=startx+cpu_perc
+    endy=starty
+
     cairo_set_line_width (cr,line_width)
     cairo_set_line_cap  (cr, line_cap)
     cairo_set_source_rgba (cr,red,green,blue,alpha)
     cairo_move_to (cr,startx,starty)
-    cairo_line_to(cr, 500, 100)
+    cairo_line_to(cr, endx, endy)
+
     cairo_stroke(cr)
 
 --    if updates>5 then
